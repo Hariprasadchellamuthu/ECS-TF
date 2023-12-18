fe7e2a9ef378b4ccf940da64ba59388b36058f1b
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-12-18 21:26:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,10 +136,8 @@ resource "aws_launch_configuration" "ecs_launch_configuration" {
   image_id             = "ami-0aee0743bf2e81172"  # Replace with your AMI ID
   instance_type        = "t2.small"  # Choose instance type as per your requirements
   associate_public_ip_address = true
-
-  network_configuration {
-    subnets = [aws_subnet.ecs_subnet.id]  # Replace with your subnet ID
-    security_groups = [aws_security_group.ecs_security_group.id]  # Reference the created security group
+  subnets = [aws_subnet.ecs_subnet.id]  # Replace with your subnet ID
+  security_groups = [aws_security_group.ecs_security_group.id]  # Reference the created security group
   }
 
   # Other configurations for the launch configuration as needed
