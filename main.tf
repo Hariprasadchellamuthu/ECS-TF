7010ae2069d1139ae5765215dfd89b8ea9491c8c
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-12-22 15:26:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,33 @@ variable "vpc_zone_identifier" {
   default     = ["subnet-09153db740467e15a", "subnet-01221c2705b0046bd", "subnet-01a2a1e8a4bea1176"] # Set your default subnet IDs here
 }
 
+resource "aws_security_group" "ecs_security_group" {
+  name        = "ecs-security-group"
+  description = "Security group for ECS tasks"
+  vpc_id      = "vpc-0405817222cfcf446" # Replace with your VPC ID
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_ecs_cluster" "jenkins_cluster" {
   name = "jenkins-ecs-cluster"
 }
@@ -36,31 +63,4 @@ resource "aws_autoscaling_group" "jenkins_autoscaling_group" {
 
   launch_configuration = aws_launch_configuration.jenkins_launch_configuration.id
   vpc_zone_identifier  = var.vpc_zone_identifier # Replace with your subnet ID
-}
-
-resource "aws_security_group" "ecs_security_group" {
-  name        = "ecs-security-group"
-  description = "Security group for ECS tasks"
-  vpc_id      = "vpc-0405817222cfcf446" # Replace with your VPC ID
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
