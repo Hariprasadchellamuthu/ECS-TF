ede119ed85b0962b0a0af4bcfb4439915cd712b7
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-12-28 12:46:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -173,6 +173,9 @@ resource "aws_ecs_task_definition" "jenkins_task_definition" {
   execution_role_arn = aws_iam_role.ecs_execution_role.arn
   task_role_arn      = aws_iam_role.ecs_task_role.arn
 
+  cpu    = "512"  # Define the CPU here
+  memory = "1024" # Define the memory here
+
   container_definitions = jsonencode([
     {
       name  = "jenkins-container"
