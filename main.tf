5b5a9a531687a7e392066ea6a51c57b2a53f6be4
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-12-28 12:38:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "aws_ecs_cluster_capacity_providers" "ecs_capacity_provider" {
   capacity_provider = ["FARGATE"]
 
   default_capacity_provider_strategy {
-    capcity_provider = "FARGATE"
+    capacity_provider = "FARGATE"
   }
 }
 
