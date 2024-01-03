e700ca65abc5badba29acac6c94135b7119c922a
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2024-01-03 16:25:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,224 +1,240 @@
-  provider "aws" {
-  region = "ap-south-1"
-}
-
-variable "vpc_zone_identifier" {
-  type        = list(string)
-  description = "List of VPC subnet IDs"
-  default     = ["subnet-09153db740467e15a", "subnet-01221c2705b0046bd", "subnet-01a2a1e8a4bea1176"] # Set your default subnet IDs here
-}
-
-resource "aws_security_group" "ecs_security_group" {
-  name        = "ecs-security-group"
-  description = "Security group for ECS tasks"
-  vpc_id      = "vpc-0405817222cfcf446" # Replace with your VPC ID
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+ provider "aws" {
+    region = "ap-south-1"
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  variable "vpc_zone_identifier" {
+    type        = list(string)
+    description = "List of VPC subnet IDs"
+    default     = ["subnet-09153db740467e15a", "subnet-01221c2705b0046bd", "subnet-01a2a1e8a4bea1176"] # Set your default subnet IDs here
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+  resource "aws_security_group" "ecs_security_group" {
+    name        = "ecs-security-group"
+    description = "Security group for ECS tasks"
+    vpc_id      = "vpc-0405817222cfcf446" # Replace with your VPC ID
 
-resource "aws_iam_role" "ecs_execution_role" {
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [
-      {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+    ingress {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 
-resource "aws_iam_role_policy" "ecs_execution_role_policy" {
-  name   = "ecs_execution_role_policy"
-  role   = aws_iam_role.ecs_execution_role.id
+    ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "ec2:CreateTags",
-          "ec2:RunInstances",
-          "ec2:StopInstances",
-          "ec2:StartInstances",
-          "ec2:TerminateInstances",
-          # Add other EC2 related actions as necessary
-        ],
-        Resource = "*",
-      },
-    ],
-  })
-}
-
-
-resource "aws_iam_role" "ecs_task_role" {
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [
-      {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "ecs_task_role_policy" {
-  name   = "ecs_task_role_policy"
-  role   = aws_iam_role.ecs_task_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "iam:PassRole",
-          "iam:CreateRole",
-          "iam:AttachRolePolicy",
-          "iam:PutRolePolicy",
-          "iam:DeleteRolePolicy",
-          "iam:DeleteRole",
-          # Add other IAM related actions as necessary
-          "ec2:DescribeSecurityGroups",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:RevokeSecurityGroupIngress",
-          # Add other security group related actions as necessary
-        ],
-        Resource = "*",
-      },
-    ],
-  })
-}
-
-data "aws_iam_policy_document" "ecs_agent" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+    egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
-}
 
-resource "aws_iam_role" "ecs_agent" {
-  name               = "ecs-agent"
-  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
-}
-
-
-resource "aws_iam_role_policy_attachment" "ecs_agent" {
-  role       = aws_iam_role.ecs_agent.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-resource "aws_iam_instance_profile" "ecs_agent" {
-  name = "ecs-agent"
-  role = aws_iam_role.ecs_agent.name
-}
-
-resource "aws_ecs_cluster" "jenkins_cluster" {
-  name = "jenkins-ecs-cluster"
-}
-
-
-
-#ECS capacity provider
-
-resource "aws_ecs_cluster_capacity_providers" "ecs_capacity_provider" {
-  cluster_name = aws_ecs_cluster.jenkins_cluster.name
-
-  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
-
-  default_capacity_provider_strategy {
-    capacity_provider = "FARGATE"
-  }
-  default_capacity_provider_strategy {
-    capacity_provider = "FARGATE_SPOT"
-  }
-}
-
-
-
-# ECS Task Definition for Jenkins
-resource "aws_ecs_task_definition" "jenkins_task_definition" {
-  family                   = "jenkins-task-family"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-
-  execution_role_arn = aws_iam_role.ecs_execution_role.arn
-  task_role_arn      = aws_iam_role.ecs_task_role.arn
-
-  cpu    = "512"  # Define the CPU here
-  memory = "1024" # Define the memory here
-
-  container_definitions = jsonencode([
-    {
-      name  = "jenkins-container"
-      image = "jenkins/jenkins:lts"
-      cpu   = 512
-      memory = 1024
-      essential = true
-      portMappings = [
+  resource "aws_iam_role" "ecs_execution_role" {
+    assume_role_policy = jsonencode({
+      Version   = "2012-10-17",
+      Statement = [
         {
-          containerPort = 8080,
-          hostPort      = 8080
-        },
+          Action    = "sts:AssumeRole",
+          Effect    = "Allow",
+          Principal = {
+            Service = "ecs-tasks.amazonaws.com"
+          }
+        }
       ]
+    })
+  }
+
+  resource "aws_iam_role_policy" "ecs_execution_role_policy" {
+    name   = "ecs_execution_role_policy"
+    role   = aws_iam_role.ecs_execution_role.id
+
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Effect   = "Allow",
+          Action   = [
+            "ec2:CreateTags",
+            "ec2:RunInstances",
+            "ec2:StopInstances",
+            "ec2:StartInstances",
+            "ec2:TerminateInstances",
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+            # Add other EC2 related actions as necessary
+          ],
+          Resource = "*",
+        },
+      ],
+    })
+  }
+
+
+  resource "aws_iam_role" "ecs_task_role" {
+    assume_role_policy = jsonencode({
+      Version   = "2012-10-17",
+      Statement = [
+        {
+          Action    = "sts:AssumeRole",
+          Effect    = "Allow",
+          Principal = {
+            Service = "ecs-tasks.amazonaws.com"
+          }
+        }
+      ]
+    })
+  }
+
+  resource "aws_iam_role_policy" "ecs_task_role_policy" {
+    name   = "ecs_task_role_policy"
+    role   = aws_iam_role.ecs_task_role.id
+
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Effect   = "Allow",
+          Action   = [
+            "iam:PassRole",
+            "iam:CreateRole",
+            "iam:AttachRolePolicy",
+            "iam:PutRolePolicy",
+            "iam:DeleteRolePolicy",
+            "iam:DeleteRole",
+            # Add other IAM related actions as necessary
+            "ec2:DescribeSecurityGroups",
+            "ec2:AuthorizeSecurityGroupIngress",
+            "ec2:RevokeSecurityGroupIngress",
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+            # Add other security group related actions as necessary
+          ],
+          Resource = "*",
+        },
+      ],
+    })
+  }
+
+  data "aws_iam_policy_document" "ecs_agent" {
+    statement {
+      actions = ["sts:AssumeRole"]
+
+      principals {
+        type        = "Service"
+        identifiers = ["ec2.amazonaws.com"]
+      }
     }
-  ])
-
-}
-
-# ECS Service for Jenkins
-resource "aws_ecs_service" "jenkins_ecs_service" {
-  name            = "jenkins-ecs-service"
-  cluster         = aws_ecs_cluster.jenkins_cluster.id
-  task_definition = aws_ecs_task_definition.jenkins_task_definition.arn
-  launch_type     = "FARGATE"
-
-
-  network_configuration {
-    subnets          = var.vpc_zone_identifier
-    security_groups  = [aws_security_group.ecs_security_group.id]
-    assign_public_ip = true
   }
-  deployment_controller {
-    type = "ECS"
+
+  resource "aws_iam_role" "ecs_agent" {
+    name               = "ecs-agent"
+    assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
   }
 
 
-  lifecycle {
-    create_before_destroy = true
+  resource "aws_iam_role_policy_attachment" "ecs_agent" {
+    role       = aws_iam_role.ecs_agent.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
   }
 
-  depends_on = [aws_ecs_task_definition.jenkins_task_definition]
-}
+  resource "aws_iam_instance_profile" "ecs_agent" {
+    name = "ecs-agent"
+    role = aws_iam_role.ecs_agent.name
+  }
 
+  resource "aws_ecs_cluster" "jenkins_cluster" {
+    name = "jenkins-ecs-cluster"
+  }
+
+
+
+  #ECS capacity provider
+
+  resource "aws_ecs_cluster_capacity_providers" "ecs_capacity_provider" {
+    cluster_name = aws_ecs_cluster.jenkins_cluster.name
+
+    capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+    default_capacity_provider_strategy {
+      capacity_provider = "FARGATE"
+    }
+    default_capacity_provider_strategy {
+      capacity_provider = "FARGATE_SPOT"
+    }
+  }
+
+
+
+  # ECS Task Definition for Jenkins
+  resource "aws_ecs_task_definition" "jenkins_task_definition" {
+    family                   = "jenkins-task-family"
+    network_mode             = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+
+    execution_role_arn = aws_iam_role.ecs_execution_role.arn
+    task_role_arn      = aws_iam_role.ecs_task_role.arn
+
+    cpu    = "512"  # Define the CPU here
+    memory = "1024" # Define the memory here
+
+    container_definitions = jsonencode([
+      {
+        name  = "jenkins-container"
+        image = "jenkins/jenkins:lts"
+        cpu   = 512
+        memory = 1024
+        essential = true
+        portMappings = [
+          {
+            containerPort = 8080,
+            hostPort      = 8080
+          },
+        ],
+        log_configuration = {
+          logDriver = "awslogs",
+          options = {
+            "awslogs-create-group"  = true,
+            "awslogs-group"         = "/ecs/my-log-group",
+            "awslogs-region"        = "ap-south-1",  
+            "awslogs-stream-prefix" = "ecs",
+          }
+        } 
+      }
+    ])
+
+    }
+
+  # ECS Service for Jenkins
+  resource "aws_ecs_service" "jenkins_ecs_service" {
+    name            = "jenkins-ecs-service"
+    cluster         = aws_ecs_cluster.jenkins_cluster.id
+    task_definition = aws_ecs_task_definition.jenkins_task_definition.arn
+    launch_type     = "FARGATE"
+
+
+    network_configuration {
+      subnets          = var.vpc_zone_identifier
+      security_groups  = [aws_security_group.ecs_security_group.id]
+      assign_public_ip = true
+    }
+    deployment_controller {
+      type = "ECS"
+    }
+
+    desired_count = 1
+
+
+    lifecycle {
+      create_before_destroy = true
+    }
+
+    depends_on = [aws_ecs_task_definition.jenkins_task_definition]
+  }
